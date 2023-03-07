662b48be97d70fad44e1e596acd7b2e37c27964d
marcelo-azcona <marceloazcona86@gmail.com>
2023-03-07 20:44:14 -0300

    Loading starting files

---
+++ src/test/java/com/marceloazcona/springbootwebtodoapp/SpringbootWebTodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloazcona.springbootwebtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebTodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
