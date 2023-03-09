372500c2c19d37fae85b6766c343b59189cbdfa0
marcelo-azcona <marceloazcona86@gmail.com>
2023-03-09 20:45:32 -0300

    Updated the addNewTodo method to update the view with new todos

--- src/main/java/com/marceloazcona/springbootwebtodoapp/todo/TodoController.java
+++ src/main/java/com/marceloazcona/springbootwebtodoapp/todo/TodoController.java
@@ -1,11 +1,13 @@
 package com.marceloazcona.springbootwebtodoapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -33,7 +35,9 @@ public class TodoController {
 	}
 
 	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
-	public String addNewTodo() {
+	public String addNewTodo(@RequestParam String description, ModelMap model) {
+		todoService.addTodo((String) model.get("name"), description, LocalDate.now(), false);
+
 		return "redirect:list-todos";
 	}
 
