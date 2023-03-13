66e50cd6129e664a093c9d8bcbcf9975f854d1bc
marcelo-azcona <marceloazcona86@gmail.com>
2023-03-13 19:41:13 -0300

    Adding and validation example

--- src/main/java/com/marceloazcona/springbootwebtodoapp/todo/TodoController.java
+++ src/main/java/com/marceloazcona/springbootwebtodoapp/todo/TodoController.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -30,13 +32,25 @@ public class TodoController {
 	}
 
 	@RequestMapping(value = "add-todo", method = RequestMethod.GET)
-	public String showNewTodoPage() {
+	public String showNewTodoPage(ModelMap model) {
+		String username = (String) model.get("name");
+
+		Todo todo = new Todo(0, username, "", LocalDate.now(), false);
+
+		model.put("todo", todo);
+
 		return "todo";
 	}
 
 	@RequestMapping(value = "add-todo", method = RequestMethod.POST)
-	public String addNewTodo(@RequestParam String description, ModelMap model) {
-		todoService.addTodo((String) model.get("name"), description, LocalDate.now(), false);
+	public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "todo";
+		}
+
+		String username = (String) model.get("name");
+		todoService.addTodo(username, todo.getDescription(), LocalDate.now(), false);
 
 		return "redirect:list-todos";
 	}
--- src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
@@ -1,4 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 <html>
 <head>
@@ -10,10 +11,14 @@
 
 	<div class="container">
 	<h2>Add todo details</h2>
-		<form method="post">
-			Description: <input type="text" name="description" /> <input
-				type="submit" class="btn btn-success" />
-		</form>
+		<form:form method="post" modelAttribute="todo">
+			Description: 
+			<form:input type="text" path="description" required="required"/>
+			<form:errors path="description" cssClass="text-warning"/>
+			<form:input type="hidden" path="id" />
+			<form:input type="hidden" path="done" />
+			<input type="submit" class="btn btn-success" />
+		</form:form>
 
 	</div>
 	<script src="webjars/bootstrap/5.1.3/js/bootstrap.min.js"></script>
